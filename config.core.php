d086aa4b1eefdb1ce5a9d6f783d518fa86743f59
Joost Brommert <info@webbiker.nl>
2015-12-22 22:02:09 +0100

    Download original website

---
+++ config.core.php
@@ -0,0 +1,4 @@
+<?php
+define('MODX_CORE_PATH', '/Users/joostbrommert/Sites/GitHub/Klompenschuurtje/core/');
+define('MODX_CONFIG_KEY', 'config');
+?>
---
+++ scripts/functions.js
@@ -0,0 +1,34 @@
+// JavaScript Document
+
+$(document).ready(function(){
+	//jQuery Cycle Carrousel on the homepage
+	$('.slides').cycle({
+		fx: 'fade',
+		pause: 1,
+		timeout: 6000
+	});
+
+	//jQuery Cycle Carrousel on detailpage
+	$('.detailfotos UL').cycle({
+		fx: 'fade',
+		timeout: 0,
+		next: "#next"
+	});
+
+	//Styling of the images on the sidebar of a content template
+	$('.OneColumnLayout .sidebar IMG').each(function(){
+		var alt = $(this).attr('alt');
+		$(this).wrap('<div class="imgPlaceHolder">').before('<div class="mask"></div>');
+		if(alt!=""){
+			$(this).after('<p>'+alt+'</p>');
+		}
+	});
+
+
+	//remove bullit after last menu item
+	$('UL.navigation LI.last').next().remove();
+
+	//add a class to all odd LI's in the Overview UL
+	$('UL.overview LI:even').addClass('margin');
+
+});
